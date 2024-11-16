3686ab406f82d34e57f6d2481dd92d49bfea2064
RakibulHasanguddu <rakibulhasanguddu@gmail.com>
2024-11-16 19:38:12 +0600

    add

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,6 +1,17 @@
 
 
 const Login = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	  } = useForm()
+	
+	  const onSubmit = (data) => console.log(data)
+	
+	  console.log(watch("example")) // watch input value by passing the name of it
+	
     return (
         <div className="flex justify-center items-center mt-10   h-[800px] bg-[url('https://i.postimg.cc/0jZr66hR/pexels-pixabay-264636.jpg')]">
             <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-50 bg-transparent-500 dark:text-gray-800">
@@ -8,18 +19,22 @@ const Login = () => {
 		<h1 className="my-3 text-4xl font-bold">Sign in</h1>
 		<p className="text-sm dark:text-gray-600">Sign in to access your account</p>
 	</div>
-	<form noValidate="" action="" className="space-y-12">
+	<form noValidate="" action="" className="space-y-12" onSubmit={handleSubmit(onSubmit)}>
 		<div className="space-y-4">
 			<div>
 				<label htmlFor="email" className="block mb-2 text-sm">Email address</label>
-				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
+				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"{...register("email", { required: true })} />
+				{errors.exampleRequired && <span>This field is required</span>}
+
 			</div>
 			<div>
 				<div className="flex justify-between mb-2">
 					<label htmlFor="password" className="text-sm">Password</label>
 					<a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-600">Forgot password?</a>
 				</div>
-				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" {...register("email", { required: true })}/>
+				{errors.password && <span>This field is required</span>}
+
 			</div>
 		</div>
 		<div className="space-y-2">
@@ -30,6 +45,8 @@ const Login = () => {
 				<a rel="noopener noreferrer" href="/regester" className="hover:underline dark:text-violet-600">Regestration</a>.
 			</p>
 		</div>
+		
+		<input type="submit" />
 	</form>
 </div>
         </div>
