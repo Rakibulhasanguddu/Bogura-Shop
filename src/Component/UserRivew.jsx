6339089f3d1467febe2b83aa98fa791bfd653598
RakibulHasanguddu <rakibulhasanguddu@gmail.com>
2024-11-16 19:52:43 +0600

    ad

---
+++ src/Component/UserRivew.jsx
@@ -0,0 +1,15 @@
+import Review from "./Review";
+
+
+const UserRivew = () => {
+    return (
+        <div className="flex justify-between gap-2">
+            <Review></Review>
+            <Review></Review>
+            <Review></Review>
+         
+        </div>
+    );
+};
+
+export default UserRivew;
